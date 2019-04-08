7b265e2c7691dcbf3ae5f29d5c3ab1e95646b343
stefansor <ssorobay00@stuy.edu>
2019-04-08 14:47:51 -0400

    tested with Mr. Ks driver but smth can always be wrong

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -117,17 +117,40 @@ public class ExpressionTree{
   }
 
   public static void main(String[] args){
-    ExpressionTree one = new ExpressionTree(4.0);
-    ExpressionTree two = new ExpressionTree(4.0);
-    ExpressionTree three = new ExpressionTree('+', one, two);
-    ExpressionTree four = new ExpressionTree(3.0);
-    ExpressionTree five = new ExpressionTree('*', four, three);
+    //ugly main sorry!
+    ExpressionTree a = new ExpressionTree(4.0);
+    ExpressionTree b = new ExpressionTree(2.0);
+
+    ExpressionTree c = new ExpressionTree('+',a,b);
+    System.out.println(c);
+    System.out.println(c.toStringPostfix());
+    System.out.println(c.toStringPrefix());
+    System.out.println(c.evaluate());//6.0
 
 
-    System.out.println(five.toStringPrefix());
-    System.out.println(five.toStringPostfix());
-    System.out.println(five);
-    System.out.println(five.evaluate());
+    ExpressionTree d = new ExpressionTree('*',c,new ExpressionTree(3.5));
+    System.out.println(d);
+    System.out.println(d.toStringPostfix());
+    System.out.println(d.toStringPrefix());
+    System.out.println(d.evaluate());//21
+
+    ExpressionTree ex = new ExpressionTree('-',d,new ExpressionTree(1.0));
+    System.out.println(ex);
+    System.out.println(ex.toStringPostfix());
+    System.out.println(ex.toStringPrefix());
+    System.out.println(ex.evaluate());//20
+
+    ex = new ExpressionTree('+',new ExpressionTree(1.0),ex);
+    System.out.println(ex);
+    System.out.println(ex.toStringPostfix());
+    System.out.println(ex.toStringPrefix());
+    System.out.println(ex.evaluate());//21
+
+    ex = new ExpressionTree('/',ex,new ExpressionTree(2.0));
+    System.out.println(ex);
+    System.out.println(ex.toStringPostfix());
+    System.out.println(ex.toStringPrefix());
+    System.out.println(ex.evaluate());//10.5
   }
 
 }
